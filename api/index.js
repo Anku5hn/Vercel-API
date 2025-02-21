f0a4ca890bbcb097baf7729850c1e58c27606947
Anku5x <ankushnagdev@outlook.com>
2025-02-22 02:11:20 +0400

    v1

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ app.use('/api/v1/read', ReadRoute);//endpoint for reading
 app.use('/api/v1/update',UpdateRoute);//endpoint for updating
 app.use('/api/v1/delete', DeleteRoute);//endpoint for deleting
 const PORT = process.env.PORT || 8080;
-app.get('*', (req, res)=>{
+app.get('/', (req, res)=>{
     res.send("Server is running");
 })
 ConnectDB().then(()=>{
