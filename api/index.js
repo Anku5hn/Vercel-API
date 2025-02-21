392b2434ecc2edc6056eb098043cc4f17b11e3e9
Anku5hn <162043513+Anku5hn@users.noreply.github.com>
2025-02-22 04:04:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,11 @@ import {UpdateRoute} from './routes/UpdateRoute.js';
 import {DeleteRoute} from './routes/DeleteRoute.js'
 dotenv.config();
 const app = express();
-app.use(cors()); 
+app.use(cors({
+  origin: ["https://ankush-io-tech-frontend.vercel.app/"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/v1/create', CreateRoute);//endpoint for creating
